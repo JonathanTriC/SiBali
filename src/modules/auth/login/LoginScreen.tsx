9c79f598a4d4046a60e9c9a6647cd7ef173990aa
Jonathan Tri <jonathantric@gmail.com>
2026-03-05 10:46:02 +0700

    chore: setup font family and icons

--- src/modules/auth/login/LoginScreen.tsx
+++ src/modules/auth/login/LoginScreen.tsx
@@ -1,4 +1,5 @@
 import { useNavigate } from '@hooks/navigation-hooks';
+import MaterialDesignIcons from '@react-native-vector-icons/material-design-icons';
 import React from 'react';
 import { Text, TouchableOpacity, View } from 'react-native';
 
@@ -6,7 +7,14 @@ const LoginScreen: React.FC = () => {
   const { navigateScreen } = useNavigate();
   return (
     <View>
-      <Text>Welcome to Login Screen</Text>
+      <Text style={{ fontFamily: 'Mulish-Light' }}>
+        Welcome to Login Screen Light
+      </Text>
+      <Text style={{ fontFamily: 'Mulish' }}>Welcome to Login Screen</Text>
+      <Text style={{ fontFamily: 'Mulish-Bold' }}>
+        Welcome to Login Screen Bold
+      </Text>
+      <MaterialDesignIcons name="home" size={24} color="#000" />
 
       <TouchableOpacity
         onPress={() => navigateScreen('Auth', { screen: 'RegisterScreen' })}
