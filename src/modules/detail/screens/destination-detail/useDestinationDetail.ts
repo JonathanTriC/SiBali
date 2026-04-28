15ffa9e8444a72dc9c645e5542e2d87b12e41c11
Jonathan Tri <jonathantric@gmail.com>
2026-04-28 15:10:05 +0700

    feat: handle open navigate

--- src/modules/detail/screens/destination-detail/useDestinationDetail.ts
+++ src/modules/detail/screens/destination-detail/useDestinationDetail.ts
@@ -6,7 +6,8 @@ import {
   useQuery,
   useQueryClient,
 } from '@tanstack/react-query';
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
+import { Linking, Platform } from 'react-native';
 
 const useDestinationDetail = () => {
   const { getRouteParams, navigation } = useNavigate();
@@ -35,6 +36,63 @@ const useDestinationDetail = () => {
       retry: false,
     });
 
+  const extractLabelFromUrl = (url: string) => {
+    try {
+      const match = url.match(/place\/([^/@]+)/);
+      if (!match) return '';
+
+      return decodeURIComponent(match[1].replace(/\+/g, ' '));
+    } catch {
+      return '';
+    }
+  };
+
+  const openMap = async (lat: number, lng: number, label: string) => {
+    const encodedLabel = encodeURIComponent(label);
+
+    const iosUrl = `http://maps.apple.com/?ll=${lat},${lng}&q=${encodedLabel}`;
+    const androidUrl = `geo:${lat},${lng}?q=${encodedLabel}`;
+    const url = Platform.OS === 'ios' ? iosUrl : androidUrl;
+
+    const webUrl = `https://www.google.com/maps/search/?api=1&query=${encodedLabel}&ll=${lat},${lng}`;
+
+    try {
+      const canOpen = await Linking.canOpenURL(url);
+      if (canOpen) {
+        await Linking.openURL(url);
+      } else {
+        await Linking.openURL(webUrl);
+      }
+    } catch (e) {
+      console.error('Error launching map:', e);
+      await Linking.openURL(webUrl);
+    }
+  };
+
+  const handleOpenNavigate = useCallback(() => {
+    const lat = destinationDetail?.latitude;
+    const lng = destinationDetail?.longitude;
+
+    if (
+      typeof lat !== 'number' ||
+      typeof lng !== 'number' ||
+      lat === 0 ||
+      lng === 0
+    ) {
+      console.warn('Invalid coordinates, cannot open map');
+      return;
+    }
+
+    const mapLabel =
+      extractLabelFromUrl(destinationDetail?.gmaps_url ?? '') || '';
+
+    openMap(lat, lng, mapLabel);
+  }, [
+    destinationDetail?.gmaps_url,
+    destinationDetail?.latitude,
+    destinationDetail?.longitude,
+  ]);
+
   useEffect(() => {
     return () => {
       queryClient.resetQueries({
@@ -50,6 +108,7 @@ const useDestinationDetail = () => {
     destinationDetail,
     isLoadingDestinationDetail,
     setActiveTab,
+    handleOpenNavigate,
   };
 };
 
